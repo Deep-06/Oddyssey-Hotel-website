d76a1202e0a945e137fbcc10c4824e99c0f40e58
Deepak Underiya <underiyadeepak@gmail.com>
2023-07-22 22:54:08 +0530

    day 5

--- JS_files/login.js
+++ JS_files/login.js
@@ -20,21 +20,26 @@ document.addEventListener("DOMContentLoaded", function(){
     const email = document.getElementById("login_mail").value;
     const password = document.getElementById("login_pass").value;
 
-    let alert=document.getElementById("login-alert")
+    let alertElement=document.getElementById("login-alert")
 
     let arr=JSON.parse(localStorage.getItem("signupData"))||[]
 
+
     if(arr.find(e=>e.email===email &&e.password===password)){
       console.log("signed in sucessfull")
-      alert.innerText="signed in sucessfull"
-    }else{
-      console.log("wrong credentials")
-      alert.innerText="wrong credentials"
+      alertElement.innerText="signed in sucessfull"
+      Redirect()
+    }else if(email==`admin@admin.com`,password==`admin`){
+      console.log(email,password)
+      Redirect("admin")
     }
-   
+    else{
+      console.log("wrong credentials")
+      alertElement.innerText="wrong credentials"
+    }
 
-   
   
+
     // Clear the login form fields after submission 
     document.getElementById("login_mail").value = "";
     document.getElementById("login_pass").value = "";
@@ -62,6 +67,7 @@ document.addEventListener("DOMContentLoaded", function(){
       gender: gender,
       email: email,
       password: password,
+      unique_id: `${firstname}+${Math.floor(Math.random()*1e+16).toString(16)}`
     };
 
     let arr=JSON.parse(localStorage.getItem("signupData"))||[]
@@ -69,23 +75,24 @@ document.addEventListener("DOMContentLoaded", function(){
     
    const duplicate=arr.find((e)=>  e.email===signupData.email);
    const invalid_password=password===password_check? true:false
-   let alert=document.getElementById("alert")
+   let alertElement=document.getElementById("alert")
   
-   if(!duplicate && (invalid_password||password!=="") && firstname!==""&&lastname!==""&&gender!=="" && email!=="" &&password!=="",password_check!==""){
-        console.log(signupData.email)
-        arr.push(signupData) 
-        alert.innerText=`signed in sucessfully`
-       
-    }else if(!invalid_password){
-        alert.innerText=`invalid password`
-    
-    }else if(firstname===""||lastname===""||gender==""||email==""||password=="",password_check==""){
-      alert.innerText=`fill all feilds`
-    }else{
-      alert.innerText=`already signed up`
-    }
+if (!duplicate && invalid_password && firstname !== "" && lastname !== "" && gender !== "" && email !== "" && password !== "" && password_check !== "") {
+  console.log(signupData.email);
+  arr.push(signupData);
+
+// post user database to the server
+  postdata("https://mock-odyssey-api-server.onrender.com/products",signupData)
+
+  alertElement.innerText = "Signed up successfully";
+} else if (!invalid_password) {
+  alertElement.innerText = "Invalid password";
+} else {
+  alertElement.innerText = "Please fill all fields";
+}
    
     
+
     localStorage.setItem("signupData", JSON.stringify(arr));
   
 
@@ -98,3 +105,46 @@ document.addEventListener("DOMContentLoaded", function(){
     document.querySelector(".signup-password").value = "";
   }
   
+// redirect to the  admin and home page 
+  function Redirect(...user) {
+      
+    if(user=="admin"){
+    window.location.replace("./hotel.html")
+    }else{
+    window.location.replace("/index.html")
+    }
+     
+  }
+
+    //  post data to server
+     async function postdata(url,obj){
+      try{
+        let res=await fetch(url,{
+          method:"POST",
+          headers:{"Content-Type": "application/json"},
+          body:JSON.stringify(obj)
+        })
+        let data=await res.json()
+        console.log(data)
+      }catch(err){
+        console.log(err)
+      }
+     }
+
+     
+     async function getdata(url,user){
+      
+      try{
+        let res=await fetch(url,{
+          method:"GET",
+        
+        })
+        let data=await res.json()
+        console.log(data)
+      let x=data.map((item)=>{return item.country})
+       return x.find((item)=>item==user)
+      }catch(err){
+        console.log(err)
+      }
+     }
+
