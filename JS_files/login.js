2def0b9138d6a89ebed329a5ca2173247ed24226
Sankar Mutyala <119348150+Sankar-Mutyala@users.noreply.github.com>
2023-07-24 11:59:28 +0530

    Done

--- JS_files/login.js
+++ JS_files/login.js
@@ -181,6 +181,7 @@ if (!duplicate && invalid_password && firstname !== "" && lastname !== "" && gen
      if(user_res==true){
       window.location.href="/HTML/payment.html"
      }
+     
 //for admin page
     // if(admin_res==true){
     //   window.location.href="/HTML/admin.html"
